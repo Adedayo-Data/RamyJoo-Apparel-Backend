c04e47d4fc87e02a73af35a3810225e66ff91c36
Adedayo-Data <theophilusdayo@outlook.com>
2025-07-07 15:45:00 +0100

    FIX: Jwt Constant fixed

--- src/main/java/com/ramyjoo/fashionstore/security/JwtConstant.java
+++ src/main/java/com/ramyjoo/fashionstore/security/JwtConstant.java
@@ -1,14 +1,19 @@
 package com.ramyjoo.fashionstore.security;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
-
+@Component
 public class JwtConstant {
 
     @Value("${secret-key}")
-    public String SECRET_KEY;
+    private String secretKey;
+
+    public String getSecretKey() {
+        return secretKey;
+    }
 
     public static final String GET_HEADER = "Authorization";
-
 }
+
 
